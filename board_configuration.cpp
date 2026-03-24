53445df4fd9ad77eaea2fbb21d925f84e65fd4d0
qwerty <jjberlin2008@yahoo.com>
2026-03-24 19:02:23 +0800

    sync

--- board_configuration.cpp
+++ board_configuration.cpp
@@ -2,18 +2,18 @@
 #include "board_overrides.h"
 // board-specific configuration setup
 static void customBoardDefaultConfiguration() {
-        engineConfiguration->injectionPins[0] = Gpio::G7
-        engineConfiguration->injectionPins[1] = Gpio::G8
-        engineConfiguration->injectionPins[2] = Gpio::D11
-        engineConfiguration->injectionPins[3] = Gpio::D10
+        engineConfiguration->injectionPins[0] = Gpio::G7;
+        engineConfiguration->injectionPins[1] = Gpio::G8;
+        engineConfiguration->injectionPins[2] = Gpio::D11;
+        engineConfiguration->injectionPins[3] = Gpio::D10;
         
-        engineConfiguration->ignitionPins[0] = Gpio::C13
-        engineConfiguration->ignitionPins[1] = Gpio::E5
-        engineConfiguration->ignitionPins[2] = Gpio::E4
-        engineConfiguration->ignitionPins[3] = Gpio::E3
+        engineConfiguration->ignitionPins[0] = Gpio::C13;
+        engineConfiguration->ignitionPins[1] = Gpio::E5;
+        engineConfiguration->ignitionPins[2] = Gpio::E4;
+        engineConfiguration->ignitionPins[3] = Gpio::E3;
         
-        engineConfiguration->triggerInputPins[0] = Gpio::B1
-        engineConfiguration->triggerInputPins[1] = Gpio::A6
+        engineConfiguration->triggerInputPins[0] = Gpio::B1;
+        engineConfiguration->triggerInputPins[1] = Gpio::A6;
 }
 
 void setup_custom_board_overrides() {
