8a4be3699172b104dcb3287b6be41bf6ff8e58d6
qwerty <jjberlin2008@yahoo.com>
2026-03-24 18:35:29 +0800

    sync

--- board_configuration.cpp
+++ board_configuration.cpp
@@ -1,47 +1,7 @@
 #include "pch.h"
 #include "board_overrides.h"
-
-Gpio getCommsLedPin() {
-	return Gpio::Unassigned;
-}
-
-Gpio getRunningLedPin() {
-	return Gpio::Unassigned;
-}
-
-Gpio getWarningLedPin() {
-	return Gpio::Unassigned;
-}
-
 // board-specific configuration setup
 static void customBoardDefaultConfiguration() {
-    //  engineConfiguration->injectionPins[0] = Gpio::F13;
-    //  engineConfiguration->ignitionPins[0] = Gpio::E15;
-
-//      engineConfiguration->triggerInputPins[0] = Gpio::B1;
-//      engineConfiguration->triggerInputPins[1] = Gpio::Unassigned;
-
-//	engineConfiguration->map.sensor.hwChannel = EFI_ADC_3;
-
- //       engineConfiguration->clt.adcChannel = EFI_ADC_2;
-
-//	engineConfiguration->iat.adcChannel = EFI_ADC_2;
-
-
-    	// 5.6k high side/10k low side = 1.56 ratio divider
-  //  	engineConfiguration->analogInputDividerCoefficient = 1.56f;
-
-    	// 6.34k high side/ 1k low side
-//    	engineConfiguration->vbattDividerCoeff = (6.34 + 1) / 1;
-
-//	engineConfiguration->adcVcc = 3.3f;
-
-//	engineConfiguration->clt.config.bias_resistor = 2490;
-//	engineConfiguration->iat.config.bias_resistor = 2490;
-
-
-	// Battery sense on PA0
-//        engineConfiguration->vbattAdcChannel = EFI_ADC_5;
 }
 
 void setup_custom_board_overrides() {
