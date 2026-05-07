979a9561b9e499ad3fffc9c4772d94398239904e
GitHub git update Action <action@github.com>
2026-05-07 02:08:18 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_stalet.h
+++ generated/controllers/generated/rusefi_generated_stalet.h
@@ -648,6 +648,7 @@
 #define FUEL_TRIM_SIZE 4
 #define GAP_TRACKING_LENGTH 18
 #define GAUGE_CATEGORY_BOOST_CONTROL "Boost Control"
+#define GAUGE_CATEGORY_ECU_STATUS "ECU Status"
 #define GAUGE_CATEGORY_ETB "ETB more"
 #define GAUGE_CATEGORY_FUEL_MATH "Fuel: math"
 #define GAUGE_CATEGORY_SENT "Sensors - SENT"
@@ -1566,7 +1567,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges true
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 2815477815
+#define SIGNATURE_HASH 356185691
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2177,7 +2178,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type true
-#define TS_SIGNATURE "rusEFI main.2026.05.06.stalet.2815477815"
+#define TS_SIGNATURE "rusEFI main.2026.05.07.stalet.356185691"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- generated/controllers/generated/signature_stalet.h
+++ generated/controllers/generated/signature_stalet.h
@@ -2,5 +2,5 @@
 // CHeaderConsumer was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 2815477815
-#define TS_SIGNATURE "rusEFI main.2026.05.06.stalet.2815477815"
+#define SIGNATURE_HASH 356185691
+#define TS_SIGNATURE "rusEFI main.2026.05.07.stalet.356185691"
