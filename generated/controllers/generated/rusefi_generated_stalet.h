21dd26663a70741a01fa73662c7286eb957a69fd
GitHub git update Action <action@github.com>
2026-05-18 02:36:40 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_stalet.h
+++ generated/controllers/generated/rusefi_generated_stalet.h
@@ -2183,7 +2183,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type true
-#define TS_SIGNATURE "rusEFI main.2026.05.17.stalet.2288334842"
+#define TS_SIGNATURE "rusEFI main.2026.05.18.stalet.2288334842"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
