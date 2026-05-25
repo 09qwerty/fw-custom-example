848a13c56c828f8f59c067b2a53dc9bf0abe97be
GitHub git update Action <action@github.com>
2026-05-25 02:41:48 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/signature_stalet.h
+++ generated/controllers/generated/signature_stalet.h
@@ -2,5 +2,5 @@
 // CHeaderConsumer was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 2180006501
-#define TS_SIGNATURE "rusEFI main.2026.05.24.stalet.2180006501"
+#define SIGNATURE_HASH 3141950560
+#define TS_SIGNATURE "rusEFI main.2026.05.25.stalet.3141950560"
