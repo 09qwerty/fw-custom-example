a94ee5ab4c4512b9d9afc79d98ccb7260457c574
GitHub git update Action <action@github.com>
2026-09-23 02:36:50 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_stalet.h
+++ generated/controllers/generated/rusefi_generated_stalet.h
@@ -1648,7 +1648,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges true
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 2781947275
+#define SIGNATURE_HASH 1428306795
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2293,7 +2293,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type true
-#define TS_SIGNATURE "rusEFI main.2026.09.22.stalet.2781947275"
+#define TS_SIGNATURE "rusEFI main.2026.09.23.stalet.1428306795"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- generated/controllers/generated/signature_stalet.h
+++ generated/controllers/generated/signature_stalet.h
@@ -2,5 +2,5 @@
 // CHeaderConsumer was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 2781947275
-#define TS_SIGNATURE "rusEFI main.2026.09.22.stalet.2781947275"
+#define SIGNATURE_HASH 1428306795
+#define TS_SIGNATURE "rusEFI main.2026.09.23.stalet.1428306795"
